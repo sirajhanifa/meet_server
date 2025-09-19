8ee1ce8719814f84eb3647bb0aecbc67be9f40a9
sirajhanifa <sirajhanifa786@gmail.com>
2025-09-19 15:21:17 +0530

    chnagew

--- index.js
+++ index.js
@@ -2,19 +2,41 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
-const connectDB = require('./db/connect.js.js');
+const connectDB = require('./db/connect.js');
 const transcriptRoutes = require('./routes/transcriptRoutes.js');
-const socketHandler = require('./signaling/socketHandler.js');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 connectDB();
 app.use('/api', transcriptRoutes);
 
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: '*' } });
-socketHandler(io);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+io.on('connection', socket => {
+  console.log('🔌 New connection:', socket.id);
+
+  socket.on('join-room', roomId => {
+    socket.join(roomId);
+    console.log(`👥 ${socket.id} joined room ${roomId}`);
+    socket.to(roomId).emit('user-joined', socket.id);
+  });
+
+  socket.on('signal', ({ roomId, signal }) => {
+    socket.to(roomId).emit('signal', { signal, from: socket.id });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('❌ Disconnected:', socket.id);
+    socket.broadcast.emit('user-left', socket.id);
+  });
+});
 
 server.listen(5000, () => console.log('🚀 Server running on port 5000'));
