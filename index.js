5b28fea02f886cd76bf207deffcdbae3100dd080
sirajhanifa <sirajhanifa786@gmail.com>
2025-09-19 15:33:33 +0530

    chnagew

--- index.js
+++ index.js
@@ -20,21 +20,44 @@ const io = new Server(server, {
   }
 });
 
+// Store room participants
+const rooms = {};
+
 io.on('connection', socket => {
   console.log('🔌 New connection:', socket.id);
 
   socket.on('join-room', roomId => {
     socket.join(roomId);
     console.log(`👥 ${socket.id} joined room ${roomId}`);
-    socket.to(roomId).emit('user-joined', socket.id);
+
+    // Track participants
+    if (!rooms[roomId]) {
+      rooms[roomId] = [];
+    }
+    rooms[roomId].push(socket.id);
+
+    // Notify other participant
+    const otherUser = rooms[roomId].find(id => id !== socket.id);
+    if (otherUser) {
+      socket.to(otherUser).emit('user-joined', socket.id);
+    }
   });
 
-  socket.on('signal', ({ roomId, signal }) => {
-    socket.to(roomId).emit('signal', { signal, from: socket.id });
+  socket.on('signal', ({ signal, to }) => {
+    io.to(to).emit('signal', { signal, from: socket.id });
   });
 
   socket.on('disconnect', () => {
     console.log('❌ Disconnected:', socket.id);
+
+    // Remove from rooms
+    for (const roomId in rooms) {
+      rooms[roomId] = rooms[roomId].filter(id => id !== socket.id);
+      if (rooms[roomId].length === 0) {
+        delete rooms[roomId];
+      }
+    }
+
     socket.broadcast.emit('user-left', socket.id);
   });
 });
