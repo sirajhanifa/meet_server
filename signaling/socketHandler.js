670470ec3711fbe9a0f171863f1baeeb76e231f3
sirajhanifa <sirajhanifa786@gmail.com>
2025-09-19 15:42:10 +0530

    Haneef Changes

--- signaling/socketHandler.js
+++ signaling/socketHandler.js
@@ -2,8 +2,12 @@ module.exports = io => {
   io.on('connection', socket => {
     socket.on('join-room', roomId => {
       socket.join(roomId);
-      socket.to(roomId).emit('user-joined', socket.id);
+      const otherUser = [...io.sockets.adapter.rooms.get(roomId)].find(id => id !== socket.id);
+      if (otherUser) {
+        socket.to(otherUser).emit('user-joined', socket.id); // 👈 send peer ID
+      }
     });
+
 
     socket.on('signal', ({ roomId, signal }) => {
       socket.to(roomId).emit('signal', { signal, from: socket.id });
